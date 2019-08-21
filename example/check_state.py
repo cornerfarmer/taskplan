b383b6aaa78da4690177cde7c7563ae4a409b60c
domin1101 <dominik.winkelbauer@outlook.de>
2019-08-21 22:25:42 +0200

    Fixes api

--- example/check_state.py
+++ example/check_state.py
@@ -1,12 +1,25 @@
 import taskplan
 import pickle
+
+from taskconf.config.Preset import Preset
+
 try:
   from pathlib2 import Path
 except ImportError:
   from pathlib import Path
 
 api = taskplan.Api()
-path, preset = api.load_task('4ae0c923-d83a-4bfe-8772-326fb9c9fcd1')
+path, preset = api.load_task('7a808ba2-6447-4dcc-8c15-61a3c2f579e3')
+
+with open(str(path / Path("model.pk")), 'rb') as handle:
+    sum = pickle.load(handle)
+
+print(sum, preset.get_int('step'))
+
+custom_preset = Preset({"config": {"step": 10}})
+print(custom_preset.get_int("step"))
+
+path, preset = taskplan.Api.load_task_from_folder("tasks/TestTask/7a808ba2-6447-4dcc-8c15-61a3c2f579e3")
 
 with open(str(path / Path("model.pk")), 'rb') as handle:
     sum = pickle.load(handle)
--- taskplan/Api.py
+++ taskplan/Api.py
@@ -2,19 +2,35 @@ from taskplan.EventManager import EventManager
 from taskplan.Project import Project
 from taskplan.Controller import Controller
 from taskplan.ProjectManager import ProjectManager
-
+from taskconf.util.Logger import Logger
+try:
+  from pathlib2 import Path
+except ImportError:
+  from pathlib import Path
+import json
+from taskconf.config.Preset import Preset
 
 class Api:
     def __init__(self):
         self.global_config = Controller.load_global_config()
-        projects = Project.load_projects(self.global_config)
-        self.project_manager = ProjectManager(projects, EventManager())
+        self.project_manager = ProjectManager(EventManager())
+        self.project_manager.load_projects(self.global_config)
 
     def load_task(self, task_uuid):
         task = self.project_manager.find_task_by_uuid(task_uuid)
-
         return task.build_save_dir(), task.preset
+
+    @staticmethod
+    def load_task_from_folder(path):
+        with open(str(path / Path("metadata.json")), "r") as handle:
+            data = json.load(handle)
+            preset = Preset(data["preset"])
+        return path, preset
 
     def load_preset(self, project_name, preset_uuid):
         project = self.project_manager.project_by_name(project_name)
         return project.configuration.presets_by_uuid[preset_uuid]
+
+    def logger_for_task(self, task_uuid):
+        task = self.project_manager.find_task_by_uuid(task_uuid)
+        return Logger(task.build_save_dir(), "main")
